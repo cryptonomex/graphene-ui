8f1ac7a7fec6d918082cc68402c60e48702fe34e
Daniel Herda <mindphlux@gmail.com>
2015-10-18 16:31:45 +0200

    Adjusted the TimeAgo Component to adjust the time displayed with the local user's timestamp, currently only used in BlockTimeAgo component

--- web/app/components/Utility/TimeAgo.jsx
+++ web/app/components/Utility/TimeAgo.jsx
@@ -8,6 +8,8 @@ class TimeAgo extends React.Component {
 
         this.timeout = null;
         this._update = this._update.bind(this);
+        this.last_block = 0;
+        this.block_local_time = 0;
     }
 
     shouldComponentUpdate(nextProps) {
@@ -18,11 +20,13 @@ class TimeAgo extends React.Component {
     }
 
     _update() {
+
         this._clearTimeout();
         let {time} = this.props;
         if (typeof time === "string") {
             time += "+00:00";
         }
+
         let timePassed = Math.round((new Date()).getTime() - (new Date(time)).getTime()) / 1000;
         let interval;
         if (timePassed < 60) { // 60s
@@ -55,13 +59,34 @@ class TimeAgo extends React.Component {
     }
 
     render() {
+
         let {time, component} = this.props;
+
+        var block_number = ChainStore.getObject('2.1.0').get('head_block_number');
+        
+        if (block_number > this.last_block) {
+            this.last_block = block_number;
+
+            this.block_local_time = new Date();
+        }
+
         if (!time) {
             return null;
         }
         
         if (typeof time === "string" && time.indexOf("+") === -1) {
             time += "+00:00";
+        }
+
+        if ( (this.block_local_time > 0) && (this.props.block == this.last_block) )
+        { 
+            // Adjust time to user's local time
+            var tmp1 = new Date(time).getTime();
+            var tmp2 = new Date(this.block_local_time).getTime();
+
+            var diff = tmp1 - tmp2;
+            time = new Date(tmp1 - diff);
+            
         }
 
         component = component ? component : "span";
