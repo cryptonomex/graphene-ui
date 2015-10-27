ed7e35af099184ab1c9a1bd46e5258922395b2f1
valzav <i@valzav.com>
2015-10-27 13:01:18 -0400

    tried to fix TransactionConfirmStore's state bug - no luck

--- web/app/components/Blockchain/Transaction.jsx
+++ web/app/components/Blockchain/Transaction.jsx
@@ -96,7 +96,6 @@ class Transaction extends React.Component {
     render() {
         let {trx} = this.props;
         let info = null;
-        console.log("trx:", trx);
         info = [];
 
         let opCount = trx.operations.length;
