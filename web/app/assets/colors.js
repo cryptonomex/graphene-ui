1f1239938bcca60ec4f3378276ba2a0e2f899799
cass <cass@bitshares.org>
2016-02-28 22:58:23 +0100

    Dark theme color ideas 1

--- web/app/assets/colors.js
+++ web/app/assets/colors.js
@@ -6,16 +6,18 @@ module.exports = {
         positiveColor: "#6EC105",
         negativeColor: "#E1424A",
         primaryText: "#e0e0e0",
-        lightTextColor: "#fff"
+        lightTextColor: "#fff",
+        widgetBorderClor: "#FFCC00"
     },
     darkTheme: {
         bidColor: "#50D2C2",
         askColor: "#E3745B",
         callColor: "#BBBF2B",
-        positiveColor: "#6EC105",
+        positiveColor: "#84F766",
         negativeColor: "#E1424A",
         primaryText: "#e0e0e0",
-        lightTextColor: "#fff"
+        lightTextColor: "#fff",
+        widgetBorderClor: "#FFCC00"
     },
     lightTheme: {
         bidColor: "#258A14",
@@ -24,6 +26,7 @@ module.exports = {
         positiveColor: "#6EC105",
         negativeColor: "#E1424A",
         primaryText: "#43484D",
-        lightTextColor: "#43484D"
+        lightTextColor: "#43484D",
+        widgetBorderClor: "#FFCC00"
     }
 }
