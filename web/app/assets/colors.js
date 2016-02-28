d0d83554c944950b2df8313278e7198c4716b145
cass <cass@bitshares.org>
2016-02-28 23:35:49 +0100

    color tweaks

--- web/app/assets/colors.js
+++ web/app/assets/colors.js
@@ -6,18 +6,16 @@ module.exports = {
         positiveColor: "#6EC105",
         negativeColor: "#E1424A",
         primaryText: "#e0e0e0",
-        lightTextColor: "#fff",
-        widgetBorderClor: "#FFCC00"
+        lightTextColor: "#fff"
     },
     darkTheme: {
-        bidColor: "#50D2C2",
-        askColor: "#E3745B",
-        callColor: "#BBBF2B",
-        positiveColor: "#84F766",
-        negativeColor: "#E1424A",
+        bidColor: "#6EC105",
+        askColor: "#F36638",
+        callColor: "#FFCC00",
+        positiveColor: "#6EC105",
+        negativeColor: "#F36638",
         primaryText: "#e0e0e0",
-        lightTextColor: "#fff",
-        widgetBorderClor: "#FFCC00"
+        lightTextColor: "#fff"
     },
     lightTheme: {
         bidColor: "#258A14",
@@ -26,7 +24,6 @@ module.exports = {
         positiveColor: "#6EC105",
         negativeColor: "#E1424A",
         primaryText: "#43484D",
-        lightTextColor: "#43484D",
-        widgetBorderClor: "#FFCC00"
+        lightTextColor: "#43484D"
     }
 }
